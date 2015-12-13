2aa5e7b28eadb84ceeb3574002e83798ad1043ad
Raja Sekhar Reddy G <raja.genupula@yahoo.com>
2015-12-13 15:09:57 +0530

    added

---
+++ getserverinfo.py
@@ -0,0 +1,14 @@
+#####################################################################
+#	G.Raja Sekhar Reddy					#####
+#	Python Version : > 3.X					#####
+#	Use this code if you want , but if you would like to improve#
+#	Please dont hesitate					#####
+#	Thank you 						#####
+#####################################################################
+
+#!/usr/local/bin/python3.3
+import subrprocess
+
+subprocess.call("clear")
+print "Hello!! Please find server information below"
+
