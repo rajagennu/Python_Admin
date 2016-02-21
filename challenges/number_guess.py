7beb205f71b77656a70bd79b4f01f0a071922f73
rajagennu <raja.genupula@yahoo.com>
2016-02-21 18:18:04 +0530

    complete number guess program and wonderful

--- challenges/number_guess.py
+++ challenges/number_guess.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+
 import random
+import shutil
 
+def stars():
+        print ((col//2)*'*')
 
+col,line=shutil.get_terminal_size()
 
+def banner(str):
+        print (str.center((col//2),'*'))
+
+great="   Hello Welcome to Number Guess Game    "
+
+#print (great.center((col//2),'*'))
+#banner(great)
+
+banner("   Hello Welcome to Number Guess Game    ")
+stars()
+#print ((col//2)*'*')
 
 name=input("Hello whats your name: ")
+gender=input(" And Gender Please : ")
+stars()
+print ("Lets start game")
 sec_num=int(10 * random.random())+100
 #print (sec_num)
 num=0
@@ -17,7 +36,9 @@ while sec_num != num :
         elif sec_num < num:
                 print("you have entered little higher, try little lower")
 
-
 else:
-        print("aah! you finally mate it Mr.%s in %d attempts" %(name,count))
-
+    if gender=="Male":
+        title="Mr"
+    else:
+        title="Miss"
+    print("aah! you finally made it %s.%s in %d attempts" %(title,name,count))
